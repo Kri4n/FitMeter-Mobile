072f0ec8e1bbcdf5e2042f664166c735928b138e
krianlloydl <krianlloydl@gmail.com>
2025-08-30 03:53:08 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/fitmeter_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fitmeter_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(const FitMeterMobile());
+}
+
+class FitMeterMobile extends StatelessWidget {
+  const FitMeterMobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FitMeter Mobile',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 38, 39, 91),
+      appBar: AppBar(title: const Text("Profile")),
+      bottomNavigationBar: MaterialApp(),
+    );
+  }
+}
