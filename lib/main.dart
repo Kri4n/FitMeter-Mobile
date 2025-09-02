7134fe7769fb693fc7ba1d2a69140b373b56c7d9
krianlloydl <krianlloydl@gmail.com>
2025-09-02 19:43:13 +0800

    Added Ui for register page

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,15 @@
 import 'package:fitmeter_mobile/views/components/bottomnavbar.dart';
-import 'package:fitmeter_mobile/views/pages/home.dart';
-import 'package:fitmeter_mobile/views/pages/login.dart';
-import 'package:fitmeter_mobile/views/pages/register.dart';
+// import 'package:fitmeter_mobile/views/pages/home.dart';
+// import 'package:fitmeter_mobile/views/pages/login.dart';
+// import 'package:fitmeter_mobile/views/pages/register.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:go_router/go_router.dart';
+// import 'package:go_router/go_router.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
   runApp(const FitMeterMobile());
 }
-
-/// The route configuration.
-final GoRouter _router = GoRouter(
-  routes: <RouteBase>[
-    GoRoute(
-      path: '/',
-      builder: (BuildContext context, GoRouterState state) {
-        return const HomePage();
-      },
-      routes: <RouteBase>[
-        GoRoute(
-          path: '/login',
-          builder: (BuildContext context, GoRouterState state) {
-            return const LoginPage();
-          },
-        ),
-        GoRoute(
-          path: '/register',
-          builder: (BuildContext context, GoRouterState state) {
-            return const RegisterPage();
-          },
-        ),
-      ],
-    ),
-  ],
-);
 
 class FitMeterMobile extends StatelessWidget {
   const FitMeterMobile({super.key});
--- lib/views/pages/register.dart
+++ lib/views/pages/register.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatelessWidget {
@@ -24,24 +25,79 @@ class RegisterPage extends StatelessWidget {
               padding: const EdgeInsets.symmetric(
                 horizontal: 24,
               ), // left & right space
-              child: const Text(
-                "Track your fitness journey, set goals, and stay motivated every day!",
-                style: TextStyle(color: Colors.white, fontSize: 14),
-                textAlign: TextAlign.center, // keep it centered
+              child: Form(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    labelStyle: TextStyle(color: Colors.white),
+                    hintText: "Enter your email",
+                    hintStyle: TextStyle(color: Colors.white),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 2),
+                    ),
+                    prefixIcon: Icon(Icons.email, color: Colors.white),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Email is required";
+                    }
+                    return null;
+                  },
+                  cursorColor: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24,
+              ), // left & right space
+              child: Form(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    labelStyle: TextStyle(color: Colors.white),
+                    hintText: "Enter your password",
+                    hintStyle: TextStyle(color: Colors.white),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 2),
+                    ),
+                    prefixIcon: Icon(Icons.password, color: Colors.white),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Password is required";
+                    }
+                    return null;
+                  },
+                  cursorColor: Colors.white,
+                ),
               ),
             ),
             SizedBox(height: 10),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                if (kDebugMode) {
+                  print('Navigate to register page');
+                }
+              },
               style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.lightBlue,
+                backgroundColor: Colors.black54,
                 elevation: 2,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10), // round corners
                 ),
               ),
               child: const Text(
-                "Get Started",
+                "Submit",
                 style: TextStyle(color: Colors.white),
               ),
             ),
