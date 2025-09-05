38c9494e0dd5941864ca49f5c91225ffc069a91a
krianlloydl <krianlloydl@gmail.com>
2025-09-05 21:36:46 +0800

    Added add workout and logout button

--- lib/views/pages/workouts.dart
+++ lib/views/pages/workouts.dart
@@ -30,14 +30,43 @@ class _WorkoutsPageState extends State<WorkoutsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FitMeter', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blueAccent,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout, color: Colors.white),
+            onPressed: () {
+              // Add your logout logic here
+              print("Logout pressed");
+            },
+          ),
+        ],
+      ),
       backgroundColor: Colors.indigo,
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: [Text('Workouts Page')],
+          children: [
+            Text('Workouts Page', style: TextStyle(color: Colors.white)),
+          ],
         ),
       ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 20),
+        child: FloatingActionButton(
+          onPressed: () {
+            if (kDebugMode) {
+              print("Add Workout");
+            }
+          },
+          backgroundColor: Colors.blueAccent,
+          shape: const CircleBorder(),
+          child: const Icon(Icons.add, size: 30, color: Colors.white),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
